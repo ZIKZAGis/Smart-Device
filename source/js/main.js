2e7c4da7dcebe88978bb2aaac351721d0e36d73f
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-08-03 11:30:33 +0300

    добавляет адаптив подвалу и часть скрипта подвала

--- source/js/main.js
+++ source/js/main.js
@@ -4,65 +4,22 @@ const textReplacement = () => {
   }
 };
 
-textReplacement();
+const listItem = Array.from(document.querySelectorAll('.footer__block'));
 
-// const listItem = Array.from(document.querySelectorAll('.footer__item'));
+const accardion = () => {
+  if (window.innerWidth <= 769) {
+    listItem.forEach(function (e) {
+      e.addEventListener('click', () => {
+        e.classList.toggle('footer__block--opened');
+      });
+    });
+  }
+};
 
-// const accardion = () => {
-//   if (window.innerWidth <= 769) {
-//     listItem.forEach(function (e) {
-//       e.addEventListener('click', () => {
-//         e.classList.toggle('footer__item--opened');
-//       });
-//     });
-//   }
-// };
+const runsScripts = () => {
+  textReplacement();
+  accardion();
+};
 
-// accardion();
+runsScripts();
 
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-
-// ---------------------------------
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
-  iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
-});
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
