5dd94f2c8d13f8abc4bd3fd191e45f9212cf7d5a
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-08-04 11:26:00 +0300

    добавляет блок с товарами и услугами

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 const textReplacement = () => {
   if (window.innerWidth <= 769) {
     document.querySelector('.welcome a').textContent = 'бесплатная консультация';
+    document.querySelector('.products__wrapper h2').textContent = 'Товары и услуги Smart Device';
   }
 };
 
